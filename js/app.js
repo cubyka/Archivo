b86108cf075702ae44a80987e32d0c5ed6634e6a
Carlos <cbueno21@icloud.com>
2018-05-22 16:33:07 -0500

    Creación Repositorio

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(parpadear);
+//Parpadeo de titulo
+function parpadear(){ //generamos una funcion que cambie el color del titulo y llamamos a la funcion regresa.
+  $(".main-titulo").animate({
+    color: "white",
+  },"slow", regresa);
+}
+function regresa(){ //Regresa el titulo al colo original y llamamos nuevamente a la funcion parapdear para hacer un ciclo interminable.
+  $(".main-titulo").animate({
+    color: "yellow",
+  },"slow", parpadear);
+}
+// incialización de Juego
+$(".btn-inicio").click(function (){
+  for (var f = 0; f < 5; f++) { //nuestra matriz sera de 7 filas y 5 cloumnas. Por lo que hacemos un ciclo for para que nos repita el proceso de abajo 7 veces
+    for (var i = 0; i <= 7; i++) { //aqui generamos 5 numeros aleatorios y los imprimimos en cada columna
+      num = Math.floor((Math.random() * 4) + 1);
+      $(".col-"+i).append("<img src='image/"+num+".png'>");
+    }
+  }
+  $(this).text("Reiniciar"); //Cambiamos el contenido del boton
+  $(this).attr("class", "btn-reinicio");
+});
+//Reinicio de juego
+$(".btn-reinicio").click(function (){
+  location.reload();
+})

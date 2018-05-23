b8b8f3b8a65f9184f06db6ade5e4865bf01be3b7
Carlos <cbueno21@icloud.com>
2018-05-23 15:59:57 -0500

    avance2

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-$(document).ready(parpadear);
-//Parpadeo de titulo
-function parpadear(){ //generamos una funcion que cambie el color del titulo y llamamos a la funcion regresa.
-  $(".main-titulo").animate({
-    color: "white",
-  },"slow", regresa);
-}
-function regresa(){ //Regresa el titulo al color original y llamamos nuevamente a la funcion parapdear para hacer un ciclo interminable.
-  $(".main-titulo").animate({
-    color: "yellow",
-  },"slow", parpadear);
-}
-// incialización de Juego
-$(".btn-inicio").click(function (){
-  for (var f = 0; f < 5; f++) { //nuestra matriz sera de 7 filas y 5 cloumnas. Por lo que hacemos un ciclo for para que nos repita el proceso de abajo 7 veces
-    for (var i = 0; i <= 7; i++) { //aqui generamos 5 numeros aleatorios y los imprimimos en cada columna
-      num = Math.floor((Math.random() * 4) + 1);
-      $(".col-"+i).append("<img src='image/"+num+".png'>");
+$( document ).ready(function() { //Las buenas practicas nos recomiendan que al usar Jquery crear una funcion document.ready y meter todo nuestro codigo dentro de ella.
+    //Parpadeo de titulo
+    //llamamos a la funcion parpadear que ejecuta el parpadeo del titulo
+    parpadear();
+    function parpadear(){ //generamos una funcion que cambie el color del titulo y llamamos a la funcion regresa.
+      $(".main-titulo").animate({
+        color: "white",
+      },"slow", regresa);
     }
-  }
-  $(this).text("Reiniciar"); //Cambiamos el contenido del boton
-  $(this).attr("class", "btn-reinicio");
-  alert($("button").hasClass("btn-reinicio"));
-  analisisTablero();
-  console.log(res1);
-});
-// Reinicio de juego
-$(".btn-reinicio").click(function (){
-  alert($("button").hasClass("btn-reinicio"));
-  location.reload();
-});
+    function regresa(){ //Regresa el titulo al color original y llamamos nuevamente a la funcion parapdear para hacer un ciclo interminable.
+      $(".main-titulo").animate({
+        color: "yellow",
+      },"slow", parpadear);
+    };
 
-function analisisTablero (){
-  res1 = $("[class^='col-']").children("img").attr("src");
-}
+    // incialización de Juego
+    $(".btn-inicio").click(function (){
+      for (var f = 0; f < 5; f++) { //nuestra matriz sera de 7 filas y 5 cloumnas. Por lo que hacemos un ciclo for para que nos repita el proceso de abajo 7 veces
+        for (var i = 0; i <= 7; i++) { //aqui generamos 5 numeros aleatorios y los imprimimos en cada columna
+          num = Math.floor((Math.random() * 4) + 1);
+          $(".col-"+i).append("<img src='image/"+num+".png'>");
+        }
+      }
+      $(this).text("Reiniciar"); //Cambiamos el contenido del boton
+      $(this).attr("class", "btn-reinicio");
+      analisisTablero();
+      console.log(res1);
+    });
+
+    // Reinicio de juego
+    $(".btn-reinicio").click(function (){
+      location.reload();
+    });
+
+    //Analisis de dulces
+    function analisisTablero (){
+      res1 = $("[class^='col-']").children("img").attr("src");
+    }
+});
